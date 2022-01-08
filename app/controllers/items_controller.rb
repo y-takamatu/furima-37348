81071e75b4b260216933d42c159060c0493e295b
yuhi-ta <yuhi1307@gmail.com>
2022-01-08 20:39:43 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name,:description,:category_id,:condition_id,:delivery_charge_id,:prefecture_id,:transport_day_id,:price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name,:description,:category_id,:condition_id,:delivery_charge_id,:prefecture_id,:transport_day_id,:price,:image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,8 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :transport_day
   belongs_to :user
+  has_one_attached :image
+
 
   with_options numericality: { other_than: 1  , message: "can't be blank"} do
     validates :category_id
