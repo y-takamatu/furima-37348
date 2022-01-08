eb1e78470a7c8c2ce113778e9717240cb0e72156
yuhi-ta <yuhi1307@gmail.com>
2022-01-08 18:52:29 +0900

    active_hash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :transport_day
   belongs_to :user
+
+  with_options numericality: { other_than: 1  , message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_charge 
+    validates :prefecture
+    validates :transport_day
+  end
 end
