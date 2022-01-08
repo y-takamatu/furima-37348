669522111d3a75470e9d8dff7c711d1fcf5c69e0
yuhi-ta <yuhi1307@gmail.com>
2022-01-08 13:00:17 +0900

    単体テスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
 
         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates_format_of :password, with: PASSWORD_REGEX, message: ' is invalid. Include both letters and numbers' 
-        with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' } do
+        
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' } do
           validates :first_name
           validates :last_name
         end
