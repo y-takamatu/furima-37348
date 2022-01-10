7c9e5e569456e0e84cce38dcf54bd8123b5ac230
yuhi-ta <yuhi1307@gmail.com>
2022-01-10 14:36:00 +0900

    単体テスト実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,9 +12,12 @@ class Item < ApplicationRecord
     validates :name
     validates :description
     validates :image
+    validates :price
   end 
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
+  validates :price, numericality: {only_integer: true, message: "is invalid. Input half-width characters"}
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:"is out of setting range"}
+  # validates :price, presence: true
   
 
   with_options numericality: { other_than: 1  , message: "can't be blank"} do
