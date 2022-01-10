7a436b0a61f9e6c47bbe4cacfe8e9c91d21710d7
yuhi-ta <yuhi1307@gmail.com>
2022-01-10 17:58:38 +0900

    javascript実装

---
+++ app/javascript/price_calc.js
@@ -0,0 +1,11 @@
+window.addEventListener('load',() =>{
+  const inputPrice = document.getElementById("item-price");
+  const calcTax = document.getElementById("add-tax-price");
+  const calkProfit = document.getElementById("profit");
+  inputPrice.addEventListener("input",() => {
+    const inputValue = inputPrice.value;
+    const addTaxValue = Math.floor(inputValue * 0.1)
+    calcTax.innerHTML = addTaxValue
+    calkProfit.innerHTML = Math.floor(inputValue - addTaxValue)
+  })
+})
