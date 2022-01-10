9f4ee9b486a38a14c3cfc910ad079f61fbd6de28
yuhi-ta <yuhi1307@gmail.com>
2022-01-10 20:54:15 +0900

    コントローラー修正（パスを指定でitem/newに遷移できてしまう問題）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,7 +72,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include"Price is invalid. Input half-width characters"
       end
-    
+      it 'userが紐づいていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include"User must exist"
+      end
     end
   end
 end
