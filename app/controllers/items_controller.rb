601b8615bc800756c879669a97ee2f069c1a6a12
yuhi-ta <yuhi1307@gmail.com>
2022-01-11 11:57:53 +0900

    authenticate_user!の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
