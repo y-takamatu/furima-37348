ef95a7a6d7ab0f3fa29f0d6ece63aa248633c365
yuhi-ta <yuhi1307@gmail.com>
2022-01-12 18:15:02 +0900

    商品購入機能実装

---
+++ db/migrate/20220112084354_create_shippings.rb
@@ -0,0 +1,14 @@
+class CreateShippings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shippings do |t|
+      t.string  :post_code,                   null: false
+      t.integer :prefecture_id,               null: false
+      t.string  :municipalities,              null: false
+      t.string  :address,                     null: false
+      t.string  :building_name
+      t.string  :phone_number,                null: false
+      t.references  :order,                   foreign_key: true
+      t.timestamps
+    end
+  end
+end
