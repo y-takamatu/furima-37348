54bb688951566b3cc1ddc8758c059135318cda15
yuhi-ta <yuhi1307@gmail.com>
2022-01-12 12:38:14 +0900

    リファクタリング（@item = Item.find(params[:id])）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_message, only: [:show, :edit, :update]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -9,16 +10,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to action: :index unless current_user.id == @item.user.id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -40,5 +38,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
                                  :transport_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_message
+    @item = Item.find(params[:id])
   end
 end
