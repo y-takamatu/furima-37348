3c72fd2d94a9b37468c54e3e5543418cb1b1101f
yuhi-ta <yuhi1307@gmail.com>
2022-01-14 17:08:44 +0900

    クレジットカード機能導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,12 +5,13 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new(order_params)
     if @order_shipping.valid?
+      pay_item
       @order_shipping.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
@@ -19,6 +20,16 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_shipping).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :phone_number).merge(
-      user_id: current_user.id,item_id: params[:item_id])
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,24 +1,25 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :user_id,:item_id,:post_code,:prefecture_id,:municipalities,:address,:building_name,:phone_number,:order_id
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipalities, :address, :building_name, :phone_number, :order_id,
+                :token
 
   with_options presence: true do
-    validates :user_id 
+    validates :user_id
     validates :item_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id
     validates :municipalities
     validates :address
-    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "Phone number is too short"}
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'Phone number is too short' }
+    validates :token
   end
-  
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
-   
-    order = Order.create(user_id: user_id,item_id: item_id)
+    order = Order.create(user_id: user_id, item_id: item_id)
 
-    Shipping.create(post_code: post_code,prefecture_id: prefecture_id,municipalities: municipalities,address: address,phone_number: phone_number,order_id: order.id)
+    Shipping.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address,
+                    phone_number: phone_number, order_id: order.id)
   end
-
 end
